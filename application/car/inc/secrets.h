40bda1e9d9e6a505280c24016e1c028452825fc3
Nathan <nathancharlesjones@gmail.com>
2026-01-15 11:03:20 -0600

    Moved fob/src/firmware.c to car/src, Edited makefile to include fob rules and added FIRMWARE_OBJ to makefile so that it can build all of the FW.

--- application/car/inc/secrets.h
+++ application/car/inc/secrets.h
@@ -1,9 +1,10 @@
-#ifndef __CAR_SECRETS__
-#define __CAR_SECRETS__
+#ifndef __FOB_SECRETS__
+#define __FOB_SECRETS__
 
-#define CAR_SECRET 1448
-
+#define PAIRED 1
+#define PAIR_PIN "231564"
 #define CAR_ID "1447"
+#define CAR_SECRET "1448"
 
 #define PASSWORD "unlock"
 
