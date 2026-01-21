4dd6f63e2762cca4e17628187eef43ea180c01de
Nathan <nathancharlesjones@gmail.com>
2026-01-21 17:44:41 -0600

    Cleaned up redundancies and errors at top of main in fobFirmware (notably, read from flash directly into fob_state_ram, ensuring that fob state is 'zeroized' prior to use; does the fact that flash zeroizes to 0xFF cause any problems?). Unlock transaction seems to work (at least, when I spoof the car-side from Waveforms), but only once, weirdly. The second time I press the unlock button, the fob doesn't respond to the 'car's ACK. Also, flags defined in the build step don't seem to be making their way into the final binary. Might be an error in carFirmware when comparing passwords and car IDs (might not actually terminate).

--- application/src/fobFirmware.c
+++ application/src/fobFirmware.c
@@ -57,12 +57,14 @@ int main(int argc, char ** argv)
   initHardware_fob(argc, argv);
 
   FLASH_DATA fob_state_ram;
-  FLASH_DATA fob_state_flash;
-  readVar((uint8_t*)(&fob_state_flash), "fob_state");
+  //FLASH_DATA fob_state_flash;
+  //readVar((uint8_t*)(&fob_state_flash), "fob_state");
+  readVar((uint8_t*)(&fob_state_ram), "fob_state");
 
 // If paired fob, initialize the system information
 #if PAIRED == 1
-  if (fob_state_flash.paired == FLASH_UNPAIRED)
+  //if (fob_state_flash.paired == FLASH_UNPAIRED)
+  if (fob_state_ram.paired == FLASH_UNPAIRED)
   {
     strcpy((char *)(fob_state_ram.pair_info.password), PASSWORD);
     strcpy((char *)(fob_state_ram.pair_info.pin), PAIR_PIN);
@@ -77,10 +79,12 @@ int main(int argc, char ** argv)
   fob_state_ram.paired = FLASH_UNPAIRED;
 #endif
 
+  /*
   if (fob_state_flash.paired == FLASH_PAIRED)
   {
     memcpy(&fob_state_ram, &fob_state_flash, sizeof(FLASH_DATA));
   }
+  */
 
   // This will run on first boot to initialize features
   if (fob_state_ram.feature_info.num_active == 0xFF)
